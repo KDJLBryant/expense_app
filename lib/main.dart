5989dad399a0fdb3579029af28840835170edda1
KDJLBryant <kbryant6644@gmail.com>
2023-05-08 00:07:02 +0000

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:expense_app/widgets/expenses.dart';
 
-import 'package:expense_app/expenses.dart';
+var kColourScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColourScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColourScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColourScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColourScheme.primaryContainer,
+            foregroundColor: kDarkColourScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColourScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColourScheme.onPrimaryContainer,
+          foregroundColor: kColourScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColourScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColourScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColourScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
+      // themeMode: ThemeMode.system, // Default
+      home: const Expenses(),
     ),
   );
 }
